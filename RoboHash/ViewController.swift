1d7f03f70b88e79f0b9579bd6762c44ffff71b99
Navin Dev <navin.dev@safetyculture.io>
2019-08-18 13:16:25 +1000

    Adds carthage and its dependencies

--- RoboHash/ViewController.swift
+++ RoboHash/ViewController.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import Alamofire
+import ReactiveSwift
+import DifferenceKit
 
 class ViewController: UIViewController {
     let dataBase = RoboHashDataBase.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let history = SearchHistory(name: "Booo", date: Date(), image: UIImage(named: "navin.png"))
         try? dataBase.save(history)
         try? dataBase.fetchAll().map { print($0.name) }
+
 
     }
     
